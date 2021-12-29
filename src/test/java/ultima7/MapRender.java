03674412644c88cae7c82cea4a9f0245deb00806
pantinor <pantinor@gmial.com>
2021-12-28 19:07:36 -0500

    more fixes

--- src/test/java/ultima7/MapRender.java
+++ src/test/java/ultima7/MapRender.java
@@ -9,6 +9,7 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.List;
+import javax.imageio.ImageIO;
 
 public class MapRender {
 
@@ -20,11 +21,13 @@ public class MapRender {
         FileInputStream mis = new FileInputStream("c://Users//panti/Desktop//STATIC//U7MAP");
         FileInputStream sis = new FileInputStream("c://Users//panti/Desktop//STATIC//SHAPES.VGA");
         FileInputStream pis = new FileInputStream("c://Users//panti/Desktop//STATIC//PALETTES.FLX");
+        FileInputStream tis = new FileInputStream("c://Users//panti/Desktop//STATIC//TFA.DAT");
 
         ByteBuffer cbb = ByteBuffer.wrap(IOUtils.toByteArray(cis)).order(ByteOrder.LITTLE_ENDIAN);
         ByteBuffer mbb = ByteBuffer.wrap(IOUtils.toByteArray(mis)).order(ByteOrder.LITTLE_ENDIAN);
         ByteBuffer sbb = ByteBuffer.wrap(IOUtils.toByteArray(sis)).order(ByteOrder.LITTLE_ENDIAN);
         ByteBuffer pbb = ByteBuffer.wrap(IOUtils.toByteArray(pis)).order(ByteOrder.LITTLE_ENDIAN);
+        byte[] tfa = IOUtils.toByteArray(tis);
 
         Region[][] regions = new Region[12][12];
         int[][][][] chunkIds = new int[12][12][16][16];
@@ -33,7 +36,8 @@ public class MapRender {
             for (int xx = 0; xx < 12; xx++) {
                 for (int y = 0; y < 16; y++) {
                     for (int x = 0; x < 16; x++) {
-                        int id = mbb.getShort() & 0xff;
+                        int id = read2(mbb, mbb.position());
+                        mbb.getShort();
                         chunkIds[yy][xx][y][x] = id;
                     }
                 }
@@ -60,14 +64,13 @@ public class MapRender {
                         for (int tiley = 0; tiley < 16; tiley++) {
                             for (int tilex = 0; tilex < 16; tilex++) {
 
-                                int id = 16 * tiley + tilex;
                                 int shapeIndex = (cbb.get(offset + 0) & 0xff) + 256 * (cbb.get(offset + 1) & 3);
                                 int frameIndex = (cbb.get(offset + 1) >> 2) & 0x1f;
 
-                                offset += 2;
-
-                                Shape s = new Shape(id, shapeIndex, frameIndex);
+                                Shape s = new Shape(shapeIndex, frameIndex);
                                 shapes[tiley][tilex] = s;
+
+                                offset += 2;
                             }
                         }
                     }
@@ -102,7 +105,17 @@ public class MapRender {
             int offset = sbb.getInt();
             int len = sbb.getInt();
             byte[] entry = new byte[len];
-            records.add(new Record(i, offset, len, entry));
+            
+            Record rec = new Record(i, offset, len, entry);
+            records.add(rec);
+
+            rec.tfa[0] = tfa[i * 3];
+            rec.tfa[1] = tfa[i * 3 + 1];
+            rec.tfa[2] = tfa[i * 3 + 2];
+
+            rec.dims[0] = 1 + (rec.tfa[2] & 7);
+            rec.dims[1] = 1 + ((rec.tfa[2] >> 3) & 7);
+            rec.dims[2] = rec.tfa[0] >> 5;
         }
 
         for (Record rec : records) {
@@ -127,12 +140,11 @@ public class MapRender {
         settings.debug = false;
         settings.alias = false;
 
-        packAtlas(0, 149, "base-tiles", records, settings);
-//        packAtlas(150, 300, "shape-tiles-1", records, settings);
-//        packAtlas(301, 600, "shape-tiles-2", records, settings);
-//        packAtlas(601, 900, "shape-tiles-3", records, settings);
-//        packAtlas(900, 1024, "shape-tiles-4", records, settings);
-
+        //packAtlas(0, 149, "base-tiles", records, settings);
+        //packAtlas(150, 300, "shape-tiles-1", records, settings);
+        //packAtlas(301, 600, "shape-tiles-2", records, settings);
+        //packAtlas(601, 900, "shape-tiles-3", records, settings);
+        //packAtlas(900, 1024, "shape-tiles-4", records, settings);
         for (int yy = 0; yy < 12; yy++) {
             for (int xx = 0; xx < 12; xx++) {
                 Region region = regions[yy][xx];
@@ -143,7 +155,7 @@ public class MapRender {
                             for (int tilex = 0; tilex < 16; tilex++) {
                                 Shape shape = chunk.shapes[tiley][tilex];
                                 Record rec = records.get(shape.shapeIndex);
-                                //region.bi.getGraphics().drawImage(rec.frames[shape.frameIndex].bi, x * tilex, y * tiley, null);
+                                region.bi.getGraphics().drawImage(rec.frames[shape.frameIndex].bi, (16 * 8 * x) + (8 * tilex), (16 * 8 * y) + (8 * tiley), null);
                             }
                         }
                     }
@@ -154,7 +166,7 @@ public class MapRender {
         for (int yy = 0; yy < 12; yy++) {
             for (int xx = 0; xx < 12; xx++) {
                 Region region = regions[yy][xx];
-                //ImageIO.write(region.bi, "PNG", new File("target/region-" + yy + "-" + xx + ".png"));
+                ImageIO.write(region.bi, "PNG", new File("target/region-" + yy + "-" + xx + ".png"));
             }
         }
 
@@ -164,7 +176,7 @@ public class MapRender {
 
         int id;
         Chunk[][] chunks;
-        BufferedImage bi = new BufferedImage(16 * 8, 16 * 8, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage bi = new BufferedImage(16 * 16 * 8, 16 * 16 * 8, BufferedImage.TYPE_INT_ARGB);
 
         public Region(int id, Chunk[][] chunks) {
             this.id = id;
@@ -187,12 +199,10 @@ public class MapRender {
 
     private static class Shape {
 
-        int id;
         int shapeIndex;
         int frameIndex;
 
-        public Shape(int id, int shapeIndex, int frameIndex) {
-            this.id = id;
+        public Shape(int shapeIndex, int frameIndex) {
             this.shapeIndex = shapeIndex;
             this.frameIndex = frameIndex;
         }
@@ -226,6 +236,9 @@ public class MapRender {
         ByteBuffer bb;
         Frame[] frames;
 
+        int[] tfa = new int[3];
+        int[] dims = new int[3];
+
         public Record(int num, int offset, int len, byte[] data) {
             this.num = num;
             this.offset = offset;
@@ -236,9 +249,8 @@ public class MapRender {
 
         @Override
         public String toString() {
-            boolean raw = isRawChunkBits();
-            return String.format("Record %d offset [%d] len [%d] %s shapeSize [%d] offset count [%d] frames [%s]",
-                    this.num, this.offset, this.len, raw ? "RAW" : "SHP", shapeSize(), offsetCount(), frames != null ? frames.length : this.len / 8 * 8);
+            return String.format("Record %d offset [%d] len [%d] %s shapeSize [%d] offset count [%d] frames [%s] hgt [%d]",
+                    this.num, this.offset, this.len, isRawChunkBits() ? "RAW" : "SHP", shapeSize(), offsetCount(), frames != null ? frames.length : this.len / 8 * 8, dims[2]);
         }
 
         void set() {
@@ -337,6 +349,11 @@ public class MapRender {
                         this.frames[f].bi.setRGB(x, y, rgb);
                     }
                 }
+                //crop out alpha or shrink the images that are all black and empty
+                if (!isRawChunkBits()) {
+                    double[] adbl = new double[]{(double) 0, (double) 0, (double) 0, (double) 255};
+                    this.frames[f].bi = ImageTransparency.cropTransparent(adbl, this.frames[f].bi);
+                }
             }
 
         }
@@ -360,6 +377,11 @@ public class MapRender {
                 | (bb.get(idx + 1) & 0xff) << 8
                 | (bb.get(idx + 2) & 0xff) << 16
                 | (bb.get(idx + 3) & 0xff) << 24;
+    }
+    
+    private static int read2(ByteBuffer bb, int idx) {
+        return (bb.get(idx + 0) & 0xff) << 0
+                | (bb.get(idx + 1) & 0xff) << 8;
     }
 
     public static int PALETTE_DAY = 0;
